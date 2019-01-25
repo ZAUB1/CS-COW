cb0e1946128f5d4e9d9b22bb227640df50d4af0e
ZAUB1 <aub1@protonmail.ch>
2019-01-25 10:55:57 +0000

    Update msrv.py

--- src_srv/msrv.py
+++ src_srv/msrv.py
@@ -1,28 +1,38 @@
 import threading
 import socket
 import sys
-from thread import start_new_thread
+import json
+#from thread import start_new_thread
+
+print("oui")
 
 HOST = '';
 PORT = 9999;
 
 conn = None;
 
-try:
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM);
-except socket.error, msg:
-    print("Couldn't create socket. Error Code: ", str(msg[0]), "Error: ", msg[1]);
-    sys.exit(0);
+class SrvEvents:
+    def __init__(self):
+        self.sevents = {};
+
+    def RegisterServerEvent(self, n, cb):
+        self.sevents[n] = cb;
+
+    def TriggerIntervalEvent(self, n):
+        self.sevents[n]();
+
+    def TriggerGlobalClientEvent(self, n, *args):
+        conn.send(json.dumps({n: n, arg: list(args)}))
+
+Server = SrvEvents();
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM);
 
 print("Socket Created");
 
-try:
-    s.bind((HOST, PORT));
-    print("Socket port " + str(PORT));
-except socket.error, msg:
-    print("Bind Failed. Error Code: {} Error: {}".format(str(msg[0]), msg[1]));
-    sys.exit();
-
+s.bind((HOST, PORT));
+print("Socket port " + str(PORT));
+    
 s.listen(10);
 print("Listening...");
 
@@ -41,23 +51,15 @@ def client_thread(conn):
         conn.sendall("ok");
     conn.close();
 
-while True:
-    conn, addr = s.accept();
-    print("Connected to " + addr[0] + ":" + str(addr[1]));
+def srvloop():
+	while True:
+	    conn, addr = s.accept();
+	    print("Connected to " + addr[0] + ":" + str(addr[1]));
 
-    start_new_thread(client_thread, (conn,));
+	    #start_new_thread(client_thread, (conn,));
+
+mt = threading.Thread(target = srvloop);
+mt.setDaemon(true);
+mt.start();
 
 s.close();
-
-class SrvEvents:
-    def __init__(self):
-        self.sevents = [];
-
-    def strtonbr(self, str):
-
-
-    def RegisterServerEvent(self, n, cb):
-        return 1
-
-    def TriggerClientEvent(self, n):
-        return 1
