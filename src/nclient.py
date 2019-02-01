0560b3550aa1488848d7dc16b44772f40c9b0c31
ZAUB1 <aubin.luneau@icloud.com>
2019-02-01 11:58:15 +0100

    Working deamon

--- src/nclient.py
+++ src/nclient.py
@@ -1,20 +1,15 @@
 import socket
 
 def Main():
-    print("Send 'q' to exit\n")
     address = "127.0.0.1"
-    nick = raw_input("nick: ")
-
     s = socket.socket()
-    s.connect((address, 9999))
-
-    s.send("WASSUP AVEC LES BISCUITS");
+    s.connect((address, 110))
 
     while True:
         data = s.recv(1024)
         data = data.decode("UTF-8")
 
-        print(data)
+        print("Received", data)
 
     s.close()
 
--- src_srv/msrv.py
+++ src_srv/msrv.py
@@ -2,27 +2,36 @@ import threading
 import socket
 import sys
 import json
+import signal
+from threading import Thread
+#import deamon
 #from thread import start_new_thread
 
-print("oui")
-
 HOST = '';
-PORT = 9999;
+PORT = 110;
 
 conn = None;
 
 class SrvEvents:
     def __init__(self):
         self.sevents = {};
+        self.teststr = "zaejzej";
+        self.conn = None;
 
-    def RegisterServerEvent(self, n, cb):
-        self.sevents[n] = cb;
+    def RegisterServerEvent(self, n):
+        self.sevents[n] = None;
 
     def TriggerIntervalEvent(self, n):
         self.sevents[n]();
 
+    def AddEventHandler(self, n, cb):
+        print("Handled", cb)
+        #if self.sevents[n] != None:
+        self.sevents[n] = cb;
+
     def TriggerGlobalClientEvent(self, n, *args):
-        conn.send(json.dumps({n: n, arg: list(args)}))
+        self.conn.sendall(b"testesteste");
+        #conn.sendall(json.dumps({n: n, arg: list(args)}))
 
 Server = SrvEvents();
 
@@ -32,14 +41,18 @@ print("Socket Created");
 
 s.bind((HOST, PORT));
 print("Socket port " + str(PORT));
-    
+
 s.listen(10);
 print("Listening...");
 
+Server.RegisterServerEvent("srv:onconn");
+
 def client_thread(conn):
+    Server.TriggerIntervalEvent("srv:onconn");
+
     print("Yup", conn);
 
-    conn.send("WOW C'EST CONNECTE EN TABARNAKKKKK.\n");
+    conn.send(b"connected");
 
     while True:
         data = conn.recv(1024);
@@ -48,18 +61,27 @@ def client_thread(conn):
         if not data:
             break;
 
-        conn.sendall("ok");
+        conn.sendall(b"ok");
     conn.close();
 
 def srvloop():
-	while True:
-	    conn, addr = s.accept();
-	    print("Connected to " + addr[0] + ":" + str(addr[1]));
+    while True:
+        conn, addr = s.accept();
+        Server.conn = conn;
+        print("Connected to " + addr[0] + ":" + str(addr[1]));
 
-	    #start_new_thread(client_thread, (conn,));
+        #start_new_thread(client_thread, (conn,));
+        thread = Thread(target = client_thread, args = (conn, ))
+        thread.start();
+        thread.join();
 
-mt = threading.Thread(target = srvloop);
-mt.setDaemon(true);
-mt.start();
+
+def testfunc():
+    print("OUIIIi")
+    Server.TriggerGlobalClientEvent("blblbl");
+
+Server.AddEventHandler("srv:onconn", testfunc);
+
+srvloop();
 
 s.close();
