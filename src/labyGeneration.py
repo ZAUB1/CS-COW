3f87e01c7127deddb33a063c942f4839356f93bb
Volham22 <corentin.machu@orange.fr>
2019-03-01 19:16:47 +0100

    Finished generation

--- src/labyGeneration.py
+++ src/labyGeneration.py
@@ -4,16 +4,16 @@ labyrinth = [['#' for i in range(15)] for i in range(15)]
 nodes = 0
 
 # Constants positions for nodes check
-MOUV = [[2, 0], [-2, 0], [0, 2], [0, -2]]
+MOUV    = [[2, 0], [-2, 0], [0, 2], [0, -2]]
+MOUVARC = [[1, 0], [-1, 0], [0, 1], [0, -1]]
 
 def union(a, b):
     for i in range(nodes):
-        if nodeID[i] == a:
-            nodeID[i] = b
+        if nodeID[i] == b:
+            nodeID[i] = a
 
 def find(a):
-    print(a)
-    return nodeID[a - 1]
+    return nodeID[a]
 
 def inArray(line, column):
     # if 0 <= line < 15 and 0 <= column < 15 then ...
@@ -34,7 +34,7 @@ class Arc:
 for i in range(15):
     for j in range(15):
         if i % 2 == 1 and j % 2 == 1:
-            labyrinth[i][j] = nodes + 1
+            labyrinth[i][j] = nodes
             nodes += 1
 
 # Find array initialisation
@@ -52,18 +52,26 @@ for i in range(15):
                 nc = j + MOUV[k][1]
 
                 if inArray(nl, nc):
-                    a = Arc(labyrinth[i][j], labyrinth[nl][nc], i, j)
+                    a = Arc(labyrinth[i][j], labyrinth[nl][nc], i + MOUVARC[k][0], j + MOUVARC[k][1])
                     arcs.append(a)
 
 # Shuffle Arc list
 random.shuffle(arcs)
+
+# Creating arc and labyrinth
+for arc in arcs:
+    if find(arc.nodeA) != find(arc.nodeB):
+        union(arc.nodeA, arc.nodeB)
+        labyrinth[arc.line][arc.column] = '.'
+
+# Clear nodes numbers in labyrinth
+for i in range(15):
+    for j in range(15):
+        if labyrinth[i][j] != '#' and labyrinth[i][j] != '.':
+            labyrinth[i][j] = '.'
 
 # Print Array
 for i in range(15):
     for j in range(15):
         print(labyrinth[i][j], end="")
     print('\n', end="")
-
-# Debug prints
-print(nodes)
-print(nodeID)
