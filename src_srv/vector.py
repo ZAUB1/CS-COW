3f8382a87345b81ff11504ebf8038f6c1395dc5c
ZAUB1 <aubin.luneau@icloud.com>
2019-03-08 11:59:16 +0100

    Vector2 start

--- src_srv/vector.py
+++ src_srv/vector.py
@@ -1,3 +1,10 @@
+import math;
+
 class Vector2:
-    def __init__():
-        
+    def __new__(self, x, y):
+        self.x = x;
+        self.y = y;
+
+        self.length = math.sqrt(pow(self.x, 2) + pow(self.y, 2));
+
+        return [self.x, self.y];
