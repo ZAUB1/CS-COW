92fcc783bee2987bdcbb3ce1e5dce001ddc87fab
ZAUB1 <aubin.luneau@icloud.com>
2019-03-15 11:31:35 +0100

    Classesssssssssssssss

--- src_srv/player.py
+++ src_srv/player.py
@@ -1,20 +1,27 @@
+import Vector2 from vector
+from random import randint
+
 class Player:
-    def __init__():
+    def __init__(self):
         self.life = 10;
         self.actions = 2;
 
-    def SetLife(n):
+    def SetLife(self, n):
         self.life = n;
 
-    def GetLife()
+    def GetLife(self)
         return self.life;
 
-    def SetActions(n):
+    def SetActions(self, n):
         self.actions = n;
 
-    def GetActions():
+    def GetActions(self):
         return self.actions;
 
 class Cow:
-    def __init__():
-        self.pos;
+    def __init__(self, map):
+        x = randint(1, 13);
+        y = randint(1, 13);
+
+        self.pos = Vector2(x, y);
+        self.found = False;
--- src_srv/vector.py
+++ src_srv/vector.py
@@ -1,10 +1,15 @@
 import math;
 
 class Vector2:
-    def __new__(self, x, y):
+    def __init__(self, x, y):
         self.x = x;
         self.y = y;
 
         self.length = math.sqrt(pow(self.x, 2) + pow(self.y, 2));
 
+    def coords(self):
         return [self.x, self.y];
+
+    def DistanceWithVector(self, b):
+        nvec = Vector2(self.x - b.x, self.y - b.y);
+        return math.sqrt(nvec.x * nvec.x + nvec.y * nvec.y);
