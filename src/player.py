71733b003eb3e3f1d342effd06f832c294742aa8
ZAUB1 <aub1@protonmail.ch>
2019-04-05 07:05:31 +0000

    Replace old pos with Vector2

--- src/player.py
+++ src/player.py
@@ -1,11 +1,11 @@
 from vector import Vector2
 
 class Player:
-    def __init__(self, cl):
+    def __init__(self):
         self.pos = Vector2(1, 1);
         self.life = 10;
 
-        self.Client = cl;
+        self.Client = None;
 
     def move(self, x, y):
         self.pos.x = x;
@@ -17,3 +17,6 @@ class Player:
         self.life = life;
 
         self.Client.TriggerServerEvent("player:setlife", self.life);
+
+    def SetClient(self, cl):
+        self.Client = cl
