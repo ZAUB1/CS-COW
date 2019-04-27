5330acb2bc5c336ee817e0cdb9f3cb3e2248ca56
Unknown <aubin.luneau@icloud.com>
2019-04-27 12:52:07 +0200

    Build client to exe & server starter

--- build.py
+++ build.py
@@ -1,5 +1,21 @@
 import py_compile;
 import shutil;
+import subprocess;
+import os
+from platform import system;
+system = system();
+
+from contextlib import contextmanager
+import sys, os
+
+from pkgutil import iter_modules
+
+def module_exists(module_name):
+    return module_name in (name for loader, name, ispkg in iter_modules())
+
+if module_exists("pyinstaller") == False:
+    print("Installing missing package :", "pyinstaller");
+    subprocess.call(r"py -m pip install pyinstaller");
 
 SERVER_FILES = [
     "game",
@@ -9,31 +25,54 @@ SERVER_FILES = [
     "msrv"
 ]
 
-for i in SERVER_FILES:
-    py_compile.compile("./src_srv/" + i + ".py", "./build/server/" + i + ".pyc");
-
-CLIENT_FILES = [
+CLIENT_MODULES = [
     "cow",
-    "helptext",
+    "player",
     "sound",
     "vector",
-    "player",
-    "nclient"
-];
+    "helptext"
+]
 
-for i in CLIENT_FILES:
-    py_compile.compile("./src/" + i + ".py", "./build/client/" + i + ".pyc");
+for i in SERVER_FILES:
+    print("[SERVER FILES] Compiling " + i + ".py");
+    py_compile.compile("./src_srv/" + i + ".py", "./build/server/" + i + ".pyc");
+
+os.chdir("./src");
+subprocess.call(r"pyinstaller client.py --distpath ../build --workpath ../build/temp --specpath ../build/temp");
 
 try:
-    shutil.copytree("./src/images", "./build/client/images");
+    print("[ASSETS] Copying images");
+    shutil.copytree("./images", "../build/client/images");
 except shutil.Error as e:
     print(e)
 except OSError as e:
     print(e)
 
 try:
-    shutil.copytree("./src/sounds", "./build/client/sounds");
+    print("[ASSETS] Copying sounds");
+    shutil.copytree("./sounds", "../build/client/sounds");
 except shutil.Error as e:
     print(e)
 except OSError as e:
     print(e)
+
+for i in CLIENT_MODULES:
+    print("[CLIENT MODULES] Compiling " + i + ".py");
+    py_compile.compile(i + ".py", "../build/client/" + i + ".pyc");
+
+os.chdir("../build");
+print("Deleting temp files");
+shutil.rmtree("./temp");
+
+print("Creating server launcher");
+if system == 'Windows':
+    srvlaunch = open("./server/run.cmd", 'w')
+    srvlaunch.write("py -3.7 msrv.pyc");
+    srvlaunch.close();
+else:
+    srvlaunch = open("./server/run.sh", 'w')
+    srvlaunch.write("sudo py -3.7 msrv.pyc");
+    srvlaunch.close();
+
+
+print("Build completed successfully");
